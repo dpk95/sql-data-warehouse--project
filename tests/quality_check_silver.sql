013d7fff23f9e8b146f907fb95eab925aee28a99
Deepak Kumar Singh <71815067+dpk95@users.noreply.github.com>
2026-03-28 19:44:24 +0530

    Update quality_check_silver.sql

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -1,3 +1,7 @@
+===================
+	TEST-1
+	cust_info
+===================
 --Check for Nulls or Duplicates in Bronze.crm_cst_id.
 --Expectation: No Results.
 SELECT 
@@ -6,12 +10,6 @@ SELECT
 FROM Silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*)>1 OR cst_id IS NULL;
-
-
--- Checking the basis on which the priority is given
-SELECT *
-FROM Bronze.crm_cust_info
-WHERE cst_id = 29466;
 
 --Solved through CTE
 WITH cust_info AS(
@@ -42,4 +40,54 @@ FROM(
 	WHERE cst_id IS NOT NULL
 	) AS info
 WHERE Ranking = 1 
+
+
+--Checking for Unwanted Spaces.
+--Expectations : No Results.
+
+SELECT 
+	cst_lastname
+FROM Silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+SELECT 
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname ,
+	TRIM(cst_lastname) AS cst_lastname,
+	cst_gndr,
+	cst_marital_status,
+	cst_create_date
+FROM Bronze.crm_cust_info;
+
+======================
+	TEST-2
+	cust_info
+======================
+--Data Standardisation and Consistency
+--Change cst_gndr = M,F as Male & Female.
+-- Change cst_marital_status= M or S as Married and single.
+-- Use UPPER() & TRIM().
+
+Select DISTINCT cst_marital_status
+FROM Silver.crm_cust_info
+
+SELECT 
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname ,
+	TRIM(cst_lastname) AS cst_lastname,
+	CASE
+		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		ELSE 'n/a'
+		END AS cst_gndr,
+		CASE
+		WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		ELSE 'n/a'
+		END AS cst_marital_status,
+	cst_create_date
+FROM Bronze.crm_cust_info; 
+
 			

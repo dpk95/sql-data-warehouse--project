36b108e514fb191a0a72f50048325ec35d08dfb6
Deepak Kumar Singh <71815067+dpk95@users.noreply.github.com>
2026-03-28 19:35:11 +0530

    Update quality_check_silver.sql

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -1,1 +1,45 @@
+--Check for Nulls or Duplicates in Bronze.crm_cst_id.
+--Expectation: No Results.
+SELECT 
+	cst_id, 
+	COUNT (*)
+FROM Silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*)>1 OR cst_id IS NULL;
 
+
+-- Checking the basis on which the priority is given
+SELECT *
+FROM Bronze.crm_cust_info
+WHERE cst_id = 29466;
+
+--Solved through CTE
+WITH cust_info AS(
+				SELECT *,
+					ROW_NUMBER()
+					OVER(
+						PARTITION BY cst_id
+						ORDER BY cst_create_date DESC
+					) ranking
+				FROM Bronze.crm_cust_info
+	)
+DELETE 
+FROM cust_info
+where ranking>1 OR cst_id is null;
+
+-- Solved through Sub-query
+SELECT *
+FROM(
+	SELECT 
+		*,
+		ROW_NUMBER()
+			OVER(
+				PARTITION BY cst_id 
+				ORDER BY cst_create_date DESC
+				) Ranking
+	FROM 
+		Bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+	) AS info
+WHERE Ranking = 1 
+			
